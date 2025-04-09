2ae99f8858b64b2a4175e3a83dccd7cb5bb283cc
jp-mazzetto <joao.milantoni@sptech.school>
2025-04-09 00:12:47 -0300

    subindo tela de historico em react

--- teste/dashboard_eleve/src/main.jsx
+++ teste/dashboard_eleve/src/main.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Dashboard from './pages/Dashboard.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DashboardEleve from "./pages/Dashboard";
+import Historico from "./pages/Historico";
 import "./styles/style-dashboard.css";
+import "./styles/style-historico.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
+    <Router>
+      <Routes>
+        {/* Rota para o Dashboard */}
+        <Route path="/" element={<DashboardEleve />} />
+        {/* Rota para o Histórico */}
+        <Route path="/historico" element={<Historico />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );

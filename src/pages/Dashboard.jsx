034682df78b6a080871ff6174c8d4da7c20356a3
AnilmarChoque <anilmarorellana@gmail.com>
2025-05-21 17:03:17 -0300

    Refatorando código de comunicação com o back-end

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getCategoriasPrdutos } from "../utils/get";
+import { getCategoriasPrdutos, getDespesas } from "../utils/get";
 import "../styles/style-dashboard.css";
 import SideBar from "../components/SideBar";
 import ContainerCategorias from "../components/ContainerCategorias";
@@ -7,24 +7,12 @@ import ChartComponent from "../components/ChartComponent";
 import ModalGastos from "../components/ModalGastos";
 import ModalComparar from "../components/ModalComparar";
 import { getDataAtual } from "../utils/util";
-import { postDespesa, postProduto } from "../utils/post";
 
 export default function DashboardEleve() {
 
     const [dataSelecionada, setDataSelecionada] = useState(getDataAtual());
-
     const [categoriasProdutos, setCategoriasProdutos] = useState([]);
-    useEffect(() => {
-        document.title = `Dashboard`;
-        (async () => {
-            try {
-                setCategoriasProdutos(await getCategoriasPrdutos());
-            } catch (error) {
-                console.error("Erro ao carregar produtos:", error);
-            }
-        })();
-    }, []);
-
+    const [despesas, setDespesas] = useState([]);
     const [showModalSaida, setShowModalSaida] = useState(false);
     const [novaSaida, setNovaSaida] = useState({
         produto: "",
@@ -32,123 +20,18 @@ export default function DashboardEleve() {
         valor: "",
         data: getDataAtual(),
     });
-
-    const [sugestoesSaida, setSugestoesSaida] = useState([]);
-
-    const handleInputChangeSaida = (e) => {
-        const { name, value } = e.target;
-
-        setNovaSaida((prev) => ({ ...prev, [name]: value }));
-
-        if (name === "produto" && value) {
-            const todosProdutos = Object.values(categoriasProdutos).flatMap((categoria) =>
-                categoria.map((item) => item.nome)
-            );
-            const filtrados = todosProdutos.filter((produto) =>
-                produto.toLowerCase().includes(value.toLowerCase())
-            );
-            setSugestoesSaida(filtrados);
-        } else if (name === "produto") {
-            setSugestoesSaida([]);
-        }
-    };
-
-    const handleSubmitSaida = async (e) => {
-        e.preventDefault();
-
-        const valorNumerico = parseFloat(
-            novaSaida.valor.replace("R$ ", "").replace(".", "").replace(",", ".")
-        );
-
-        const novaSaidaFormatada = {
-            ...novaSaida,
-            valor: valorNumerico,
-        };
-
-        let produtoExistente = null;
-
-        Object.entries(categoriasProdutos).forEach(([categoriaNome, itens]) => {
-            const encontrado = itens.find(
-                (item) => item.nome.toLowerCase() === novaSaidaFormatada.produto.toLowerCase()
-            );
-            if (encontrado) {
-                produtoExistente = encontrado;
-            }
-        });
-
-        if (produtoExistente) {
-            const novaDespesa = [{
-                produtoId: produtoExistente.id,
-                valor: novaSaidaFormatada.valor,
-                data: novaSaidaFormatada.data,
-            }];
-
+    
+    useEffect(() => {
+        document.title = `Dashboard`;
+        (async () => {
             try {
-                const response = await postDespesa(novaDespesa);
-                console.log("Despesa criada com sucesso:", response);
+                setCategoriasProdutos(await getCategoriasPrdutos());
+                setDespesas(await getDespesas());
             } catch (error) {
-                console.error("Erro ao criar despesa:", error);
+                console.error("Erro ao carregar produtos:", error);
             }
-        } else {
-            const novoProduto = [{
-                categoriaId: parseInt(novaSaidaFormatada.categoria, 10),
-                nome: novaSaidaFormatada.produto,
-            }];
-
-            console.log("Novo produto criado:", novoProduto);
-
-            try {
-                const responseProduto = await postProduto(novoProduto);
-                console.log("Produto criado com sucesso:", responseProduto);
-
-                const categoriasAtualizadas = await getCategoriasPrdutos();
-                setCategoriasProdutos(categoriasAtualizadas);
-
-                let novoProdutoCriado = null;
-
-                Object.entries(categoriasAtualizadas).forEach(([categoriaNome, itens]) => {
-                    const novoProdutoencontrado = itens.find(
-                        (item) => item.nome.toLowerCase() === novaSaidaFormatada.produto.toLowerCase()
-                    );
-                    if (novoProdutoencontrado) {
-                        novoProdutoCriado = novoProdutoencontrado;
-                    }
-                });
-
-                console.log(novoProdutoCriado);
-
-                if (novoProdutoCriado) {
-                    const novaDespesa = [{
-                        produtoId: novoProdutoCriado.id,
-                        valor: novaSaidaFormatada.valor,
-                        data: novaSaidaFormatada.data,
-                    }];
-
-                    try {
-                        const responseDespesa = await postDespesa(novaDespesa);
-                        console.log("Despesa criada com sucesso para o novo produto:", responseDespesa);
-                    } catch (error) {
-                        console.error("Erro ao criar despesa para o novo produto:", error);
-                    }
-                } else {
-                    console.error("Erro: Novo produto não encontrado após atualização.");
-                }
-            } catch (error) {
-                console.error("Erro ao criar produto:", error);
-            }
-        }
-
-        console.log("Nova saída:", novaSaidaFormatada);
-
-        // Resetar o estado e fechar o modal
-        setShowModalSaida(false);
-        setNovaSaida({
-            produto: "",
-            categoria: "",
-            valor: "",
-            data: getDataAtual(),
-        });
-    };
+        })();
+    }, []);
 
     const [showModalComparar, setShowModalComparar] = useState(false);
 
@@ -212,18 +95,26 @@ export default function DashboardEleve() {
                         </div>
 
                         <div className="categorias-container">
-                            <ContainerCategorias tipo="saida" categorias={categoriasProdutos} dataSelecionada={dataSelecionada} />
+                            <ContainerCategorias 
+                                tipo="saida" 
+                                categorias={categoriasProdutos} 
+                                setCategorias={setCategoriasProdutos}
+                                dataSelecionada={dataSelecionada}
+                                despesas={despesas}
+                                setDespesas={setDespesas}
+                            />
                         </div>
 
                         {showModalSaida && (
                             <ModalGastos
                                 tipo="saida"
-                                submit={handleSubmitSaida}
                                 novoItem={novaSaida}
+                                setNovoItem={setNovaSaida}
                                 showModal={setShowModalSaida}
-                                change={handleInputChangeSaida}
-                                sugestoes={sugestoesSaida}
-                                setSugestoes={setSugestoesSaida}
+                                despesas={despesas}
+                                setDespesas={setDespesas}
+                                categorias={categoriasProdutos}
+                                setCategorias={setCategoriasProdutos}
                             />
                         )}
 

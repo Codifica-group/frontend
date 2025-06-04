6d35a5597529d77e5bda145b306012f07831a115
Felippe-ASLopes <felippedias3@gmail.com>
2025-06-04 17:07:28 -0300

    :recycle: refact: nome AlertErro

--- src/components/AlertErro.jsx
+++ src/components/AlertErro.jsx
@@ -1,13 +1,14 @@
 import "../styles/AlertErro.css";
+import erroIcon from "../assets/erro.png";
 
-export default function ModalErro({ mensagem = "Ocorreu um erro inesperado", erro = "", onClose }) {
+export default function AlertErro({ mensagem = "Ocorreu um erro inesperado", erro = "", onClose }) {
     return (
         <div className="modal-overlay">
             <div className="modal-erro">
                 <button className="btn-fechar" onClick={onClose} aria-label="Fechar">
                     X
                 </button>
-                <img src="src\assets\erro.png" alt="Erro" className="erro-icon" />
+                <img src={erroIcon} alt="Erro" className="erro-icon" />
                 <span className="modal-erro__mensagem">{mensagem}</span>
                 {erro && (
                     <pre className="modal-erro__detalhe">
